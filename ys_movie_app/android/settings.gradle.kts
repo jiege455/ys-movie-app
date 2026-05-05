bdfca65521e366f02c58e566e9ed6fcbb5ccc6ce
杰哥网络科技 <2711793818@qq.com>
2026-05-06 02:23:29 +0800

    fix: 修复 APK 构建失败 - better_player androidx.media 依赖找不到

--- ys_movie_app/android/settings.gradle.kts
+++ ys_movie_app/android/settings.gradle.kts
@@ -20,6 +20,18 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/releases") }
+    }
+}
+
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.11.1" apply false
